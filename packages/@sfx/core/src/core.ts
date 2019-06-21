ded77f8b547ec3b4121d1bbd29a8e92d29da89e2
Johann Tutor <johann.tutor@groupbyinc.com>
2019-06-21 17:49:17 -0400

    Call plugins' register() function on registration
    Each plugin will expose a `register` function that takes in a plugins
    object that contains a reference to all plugins and returns a value to
    expose.

--- packages/@sfx/core/src/core.ts
+++ packages/@sfx/core/src/core.ts
@@ -1,2 +1,14 @@
+import { Plugin } from './plugin';
+
 export default class Core {
+  plugins = Object.create(null);
+
+  /**
+   * Register one or more plugins with Core.
+   *
+   * @param plugins An array of plugin instances to regsiter.
+   */
+  register(plugins: Plugin[]) {
+    plugins.forEach((p) => p.register(this.plugins));
+  }
 }
--- packages/@sfx/core/test/unit/common/core.test.ts
+++ packages/@sfx/core/test/unit/common/core.test.ts
@@ -1,8 +1,35 @@
 import { expect } from 'chai';
+import { spy } from 'sinon';
 import Core from '../../../src/core';
 
 describe('Core', () => {
-  it('should be a class', () => {
-    expect(Core).to.be.a('function');
+  let core: Core;
+
+  beforeEach(() => {
+    core = new Core();
+  });
+
+  describe('constructor()', () => {
+    it('should create an empty null-prototype plugins object', () => {
+      expect(core.plugins).to.be.empty;
+      expect(Object.getPrototypeOf(core.plugins)).to.be.null;
+    });
+  });
+
+  describe('register()', () => {
+    it('should call the register() function of each plugin', () => {
+      const registerA = spy();
+      const registerB = spy();
+      const plugins = [
+        { register: registerA },
+        { register: registerB },
+      ];
+
+      core.register(plugins);
+
+      expect(registerA).to.be.calledWith(core.plugins);
+      expect(registerB).to.be.calledWith(core.plugins);
+      expect(registerB).to.be.calledAfter(registerA);
+    });
   });
 });
