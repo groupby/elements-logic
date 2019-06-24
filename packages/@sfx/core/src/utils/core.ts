1c2df93b52bf621086cd305d63850372a41cc337
Johann Tutor <johann.tutor@groupbyinc.com>
2019-06-24 10:41:36 -0400

    Return list of missing dependencies instead

--- packages/@sfx/core/src/utils/core.ts
+++ packages/@sfx/core/src/utils/core.ts
@@ -1,9 +1,7 @@
-export function checkDependencies(available: string[], required: string[]) {
+export function getMissingDependencies(available: string[], required: string[]): string[] {
   const availableSet = new Set(available);
   const requiredSet = new Set(required);
   const difference = new Set(Array.from(requiredSet).filter((p) => !availableSet.has(p)));
 
-  if (difference.size) {
-    throw new Error('Unmet dependencies: ' + Array.from(difference.values()).sort().join(', '));
-  }
+  return Array.from(difference.values()).sort();
 }
