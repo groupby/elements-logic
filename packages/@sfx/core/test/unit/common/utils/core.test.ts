5835401628275883a03d02f0a85b55d2c45b84cd
Johann Tutor <johann.tutor@groupbyinc.com>
2019-06-24 10:45:35 -0400

    Test positive dependency cases

--- packages/@sfx/core/test/unit/common/utils/core.test.ts
+++ packages/@sfx/core/test/unit/common/utils/core.test.ts
@@ -11,5 +11,23 @@ describe('CoreUtils', () => {
 
       expect(missing).to.have.members(['d', 'e']);
     });
+
+    it('should return no missing dependencies when all are met', () => {
+      const available = ['a', 'b', 'c'];
+      const required = ['a', 'b'];
+
+      const missing = getMissingDependencies(available, required);
+
+      expect(missing).to.eql([]);
+    });
+
+    it('should return no missing dependencies when there are no dependencies', () => {
+      const available = ['a', 'b', 'c'];
+      const required = [];
+
+      const missing = getMissingDependencies(available, required);
+
+      expect(missing).to.eql([]);
+    });
   });
 });
