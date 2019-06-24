439b5ff08ea2d550370233950f4669f3d5e721b7
Johann Tutor <johann.tutor@groupbyinc.com>
2019-06-24 10:38:10 -0400

    Throw error when there are unmet dependencies

--- packages/@sfx/core/src/utils/core.ts
+++ packages/@sfx/core/src/utils/core.ts
@@ -1,2 +1,9 @@
-export function checkDependencies() {
+export function checkDependencies(available: string[], required: string[]) {
+  const availableSet = new Set(available);
+  const requiredSet = new Set(required);
+  const difference = new Set(Array.from(requiredSet).filter((p) => !availableSet.has(p)));
+
+  if (difference.size) {
+    throw new Error('Unmet dependencies: ' + Array.from(difference.values()).sort().join(', '));
+  }
 }
