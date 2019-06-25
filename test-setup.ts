034f3618ab218afd9b7bc625f95ec6a9baea1b8f
Johann Tutor <johann.tutor@groupbyinc.com>
2019-06-25 14:31:50 -0400

    Restore sinon's default sandbox after each test
    This keeps tests isolated.

--- test-setup.ts
+++ test-setup.ts
@@ -1,4 +1,9 @@
 import { use } from 'chai';
+import { restore as sinonRestore } from 'sinon';
 import * as sinonChai from 'sinon-chai';
 
 use(sinonChai);
+
+afterEach(() => {
+  sinonRestore();
+});
