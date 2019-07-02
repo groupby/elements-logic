17077d1800bd932ffa16dc66db2f40092cbae7e9
JoeyDeol <gurjothdeol@gmail.com>
2019-07-02 10:52:54 -0400

    Create initial plugin file structure

---
+++ packages/@sfx/events-browser-plugin/src/index.ts
@@ -0,0 +1,16 @@
+/**
+ * This test comment _supports_ [Markdown and other fun stuff](https://typedoc.org/guides/doccomments/)
+ */
+const testObj = {
+  label: 'test text',
+}
+
+const testFunc = (text: TestInterface) => {
+  console.log(`I am logging out ${text.label}`);
+}
+
+testFunc(testObj);
+
+interface TestInterface {
+  label: string;
+}
