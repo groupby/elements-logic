acb7ab6dc0c10fea3a157673d001d0a060f4385a
Geoff Coutts <geoff.coutts@gmail.com>
2019-07-02 12:58:12 -0400

    Create top level test directory for utils and setup

--- test-setup.ts
+++
@@ -1,9 +0,0 @@
-import { use } from 'chai';
-import { restore as sinonRestore } from 'sinon';
-import * as sinonChai from 'sinon-chai';
-
-use(sinonChai);
-
-afterEach(() => {
-  sinonRestore();
-});
