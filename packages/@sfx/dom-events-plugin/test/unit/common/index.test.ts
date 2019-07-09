3c0bbb73908853142d58f7c74125229433f04678
JoeyDeol <gurjothdeol@gmail.com>
2019-07-09 14:45:19 -0400

    Create tests for exporting interfaces.

--- packages/@sfx/dom-events-plugin/test/unit/common/index.test.ts
+++ packages/@sfx/dom-events-plugin/test/unit/common/index.test.ts
@@ -1,9 +1,22 @@
-import { expect } from '../../utils';
-import { DomEventsPlugin as DomEventsPluginExport } from '../../../src';
+import { AssertTypesEqual, expect } from '../../utils';
+import {
+  DomEventsPlugin as DomEventsPluginExport,
+  DomEventsPluginOptions as DomEventsPluginOptionsExport,
+  DomEventsPluginExposedValue as DomEventsPluginExposedValueExport,
+} from '../../../src';
 import DomEventsPlugin from '../../../src/dom-events-plugin';
+import { DomEventsPluginOptions, DomEventsPluginExposedValue } from '../../../src/dom-events-plugin';
 
 describe('Entry point', () => {
   it('should export DomEventsPlugin ', () => {
     expect(DomEventsPluginExport).to.equal(DomEventsPlugin);
+  });
+
+  it('should export the DomEventsPluginOptions interface', () => {
+    const test: AssertTypesEqual<DomEventsPluginOptions , DomEventsPluginOptionsExport> = true;
+  });
+
+  it('should export the DomEventsPluginExposedValue interface', () => {
+    const test: AssertTypesEqual<DomEventsPluginExposedValue, DomEventsPluginExposedValueExport> = true;
   });
 });
