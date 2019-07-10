018ba7b064b81109776bef7392f37eb80e878490
ssbelle <shawna@Shawnas-MacBook-Pro.local>
2019-07-10 11:28:24 -0400

    Move tests to common folder and add type to register method

--- packages/@sfx/sayt-plugin/README.md
+++ packages/@sfx/sayt-plugin/README.md
@@ -12,5 +12,4 @@ core.register(saytPlugin);
 ```
 The plugin registers an instance of the [Sayt client](https://www.npmjs.com/package/sayt) with Core.
 
-
 The SaytPlugin constructor can accept options to configure the exposed Sayt client. See the [SaytConfig interface](https://github.com/groupby/sayt-client/blob/develop/src/core/sayt.ts#L77) for available options.
--- packages/@sfx/sayt-plugin/src/sayt-plugin.ts
+++ packages/@sfx/sayt-plugin/src/sayt-plugin.ts
@@ -1,21 +1,34 @@
 import { Plugin, PluginRegistry, PluginMetadata } from '@sfx/core';
 import { Sayt, SaytConfig } from 'sayt';
-
-export default class SaytPlugin implements Plugin {
+/**
+ * This plugin is responsible for exposing an instance of sayt
+ * to Core.
+ */
+ export default class SaytPlugin implements Plugin {
   get metadata(): PluginMetadata {
     return {
       name: 'sayt',
       depends: [],
     };
   }
-
+  /**
+   * The sayt property is the value that the Sayt Plugin
+   * exposes to the Core entity.
+   */
   sayt: Sayt;
-
+  /**
+   * The sayt plugin constructor function can take in optional options. It also instantiates
+   * an instance of sayt, with any given options.
+   * @param options an object that should contain subdomain and collection properties to
+   * create a valid sayt request.
+   */
   constructor(options?: SaytConfig) {
     this.sayt = new Sayt(options);
   }
-
-  register() {
+  /**
+   * The register method returns the instance of sayt.
+   */
+  register(): Sayt {
     return this.sayt;
   }
 }
