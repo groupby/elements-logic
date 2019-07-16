39904cee05a53e7008c95a4f752cc803e2b3ed4e
Geoff Coutts <geoff.coutts@gmail.com>
2019-07-16 15:20:24 -0400

    Add description to plugin.

--- packages/@sfx/sayt-driver-plugin/src/sayt-driver-plugin.ts
+++ packages/@sfx/sayt-driver-plugin/src/sayt-driver-plugin.ts
@@ -1,5 +1,11 @@
 import { Plugin, PluginRegistry, PluginMetadata } from '@sfx/core';
 
+/**
+ * Driver plugin that serves as the link between the Sayt data source
+ * and the view layer. This plugin is responsible for listening to events,
+ * sending search requests to internal data source APIs,
+ * and then emitting the API response back in an event.
+ */
 export default class SaytDriverPlugin implements Plugin {
   get metadata(): PluginMetadata {
     return {
