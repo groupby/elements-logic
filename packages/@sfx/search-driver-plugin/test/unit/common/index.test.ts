16bf03963771227591d26309d51801ad953a6aed
Daniel Moniz <daniel.moniz@groupbyinc.com>
2019-07-22 15:52:33 -0400

    Rename test to match class being tested

--- packages/@sfx/search-driver-plugin/test/unit/common/index.test.ts
+++ packages/@sfx/search-driver-plugin/test/unit/common/index.test.ts
@@ -3,7 +3,7 @@ import { default as SearchDriverPlugin } from '../../../src/search-driver-plugin
 import { SearchDriverPlugin as SearchDriverExport } from '../../../src/index';
 
 describe('Entry point', () => {
-  it('should export SaytPlugin', () => {
+  it('should export SearchDriverPlugin', () => {
     expect(SearchDriverPlugin).to.equal(SearchDriverExport);
   });
 });
