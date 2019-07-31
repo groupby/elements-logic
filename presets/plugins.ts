b317edbd9e72be2b3492344c10070812c82b9113
ssbelle <shawna@Shawnas-MacBook-Pro.local>
2019-07-31 11:39:24 -0300

    Add search driver plugin to file so that it is included in the bundle process

--- presets/plugins.ts
+++ presets/plugins.ts
@@ -2,3 +2,4 @@ export * from '@sfx/dom-events-plugin';
 export * from '@sfx/sayt-driver-plugin';
 export * from '@sfx/sayt-plugin';
 export * from '@sfx/search-plugin';
+export * from '@sfx/search-driver-plugin';
