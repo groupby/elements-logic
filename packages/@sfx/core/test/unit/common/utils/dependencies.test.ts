a1becf499e352fd3b5787bfbc5b2677cb4b78f31
Johann Tutor <johann.tutor@groupbyinc.com>
2019-09-09 11:24:00 -0400

    Test createDependencyGraph for empty object when no dependencies are given

--- packages/@sfx/core/test/unit/common/utils/dependencies.test.ts
+++ packages/@sfx/core/test/unit/common/utils/dependencies.test.ts
@@ -13,5 +13,17 @@ describe('DependencyUtils', () => {
       expect(dependencies).to.deep.equal({});
       expect(Object.getPrototypeOf(dependencies)).to.be.null;
     });
+
+    it('should create an empty object when given plugins with no dependencies', () => {
+      const plugins: any = [
+        { metadata: { name: 'a', depends: [] } },
+        { metadata: { name: 'b', depends: [] } },
+        { metadata: { name: 'c', depends: [] } },
+      ];
+
+      const dependencies = createDependencyGraph(plugins);
+
+      expect(dependencies).to.deep.equal({});
+    });
   });
 });
