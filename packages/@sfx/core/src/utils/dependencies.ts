b96ac6070748ed693a258523ecbca55dec4b6bab
Johann Tutor <johann.tutor@groupbyinc.com>
2019-09-09 11:33:22 -0400

    Type return value of createDependencyGraph

--- packages/@sfx/core/src/utils/dependencies.ts
+++ packages/@sfx/core/src/utils/dependencies.ts
@@ -1,5 +1,13 @@
 import { Plugin } from '../plugin';
 
-export function createDependencyGraph(plugins: Plugin[]) {
+export function createDependencyGraph(plugins: Plugin[]): DependencyGraph {
   return Object.create(null);
+}
+
+/**
+ * The type of the plugin dependency graph. The graph is a mapping from a
+ * plugin name to all the names of the plugins that depend on it.
+ */
+export interface DependencyGraph {
+  [name: string]: string[];
 }
