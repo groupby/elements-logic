62c59467f0b201213c4a6d852cb6706af2cdbc20
Daniel Moniz <daniel.moniz@groupbyinc.com>
2019-10-08 14:09:32 -0400

    Minor cleanup in sayt driver

--- packages/@sfx/sayt-driver-plugin/test/unit/common/sayt-driver-plugin.test.ts
+++ packages/@sfx/sayt-driver-plugin/test/unit/common/sayt-driver-plugin.test.ts
@@ -429,10 +429,10 @@ describe('Sayt Driver Plugin', () => {
 
       const result = driver.searchCallback(response);
 
-      expect(result.products[0].key1).to.equal(firstProductTitle);
-      expect(result.products[0].key2).to.equal(key2);
-      expect(result.products[1].key1).to.equal(secondProductTitle);
-      expect(result.products[1].key2).to.equal(key2);
+      expect(result.products).to.deep.equal([
+        { key1: firstProductTitle, key2 },
+        { key1: secondProductTitle, key2 },
+      ])
     });
 
     it('should filter out any products that map to falsy values', () => {
