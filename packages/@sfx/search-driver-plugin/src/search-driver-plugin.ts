e61995b1337f64b268434f33fbd07cc574b9c2ea
Daniel Moniz <daniel.moniz@groupbyinc.com>
2019-10-10 14:10:16 -0400

    Add eslint-disable line to search driver

--- packages/@sfx/search-driver-plugin/src/search-driver-plugin.ts
+++ packages/@sfx/search-driver-plugin/src/search-driver-plugin.ts
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/no-extraneous-dependencies, import/no-unresolved
 import { Plugin, PluginRegistry, PluginMetadata } from '@sfx/core';
+// eslint-disable-next-line import/no-extraneous-dependencies, import/no-unresolved
 import { Record, Results, Request as SearchRequest } from '@sfx/search-plugin';
 import {
   SEARCH_REQUEST,
