59c36b8893600e5402d53bfcfb795bd65607216f
Johann Tutor <johann.tutor@groupbyinc.com>
2019-10-24 11:06:16 -0400

    Add cache plugin skeleton

---
+++ packages/@sfx/cache/src/cache-plugin.ts
@@ -0,0 +1,7 @@
+/**
+ * The SF-X cache plugin. This plugin registers an instance of `Map` that
+ * is intended to be used by other plugins as a cache or other data
+ * store. No automatic cache clearing or expiring is performed.
+ */
+export default class CachePlugin {
+}
---
+++ packages/@sfx/cache/test/unit/common/cache-plugin.test.ts
@@ -0,0 +1,19 @@
+import { expect, spy, stub } from '../../utils';
+import CachePlugin from '../../../src/cache-plugin';
+
+describe('CachePlugin', () => {
+  let cachePlugin;
+
+  beforeEach(() => {
+    cachePlugin = new CachePlugin();
+  });
+
+  describe('metadata getter', () => {
+  });
+
+  describe('constructor()', () => {
+  });
+
+  describe('register()', () => {
+  });
+});
