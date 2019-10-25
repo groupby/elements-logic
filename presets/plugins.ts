33e38e0d949824b5a71d04f21fc2c4e515267664
Johann Tutor <johann.tutor@groupbyinc.com>
2019-10-25 16:03:38 -0400

    Add cache and cache-driver to plugins presets

--- presets/plugins.ts
+++ presets/plugins.ts
@@ -1,3 +1,5 @@
+export * from '@groupby/elements-cache-plugin';
+export * from '@groupby/elements-cache-driver-plugin';
 export * from '@groupby/elements-dom-events-plugin';
 export * from '@groupby/elements-sayt-driver-plugin';
 export * from '@groupby/elements-sayt-plugin';
