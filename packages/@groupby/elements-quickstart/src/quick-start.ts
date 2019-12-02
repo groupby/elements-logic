381af4838e9b0a3aa58d9a4fe64f104c739bcdeb
Johann Tutor <johann.tutor@groupbyinc.com>
2019-12-02 14:29:01 -0500

    Test instantiating plugins

--- packages/@groupby/elements-quickstart/src/quick-start.ts
+++ packages/@groupby/elements-quickstart/src/quick-start.ts
@@ -1,5 +1,19 @@
+import { CacheDriverPlugin } from '@groupby/elements-cache-driver-plugin';
+import { CachePlugin } from '@groupby/elements-cache-plugin';
 import { Core } from '@groupby/elements-core';
+import { DomEventsPlugin } from '@groupby/elements-dom-events-plugin';
+import { SaytDriverPlugin } from '@groupby/elements-sayt-driver-plugin';
+import { SaytPlugin } from '@groupby/elements-sayt-plugin';
+import { SearchDriverPlugin } from '@groupby/elements-search-driver-plugin';
+import { SearchPlugin } from '@groupby/elements-search-plugin';
 
 export default function quickStart(): Core {
+  const cacheDriverPlugin = new CacheDriverPlugin();
+  const cachePlugin = new CachePlugin();
+  const domEventsPlugin = new DomEventsPlugin();
+  const saytDriverPlugin = new SaytDriverPlugin();
+  const saytPlugin = new SaytPlugin();
+  const searchDriverPlugin = new SearchDriverPlugin();
+  const searchPlugin = new SearchPlugin({} as any);
   return new Core();
 }
--- packages/@groupby/elements-quickstart/test/unit/common/quick-start.test.ts
+++ packages/@groupby/elements-quickstart/test/unit/common/quick-start.test.ts
@@ -1,16 +1,56 @@
+import * as Cache from '@groupby/elements-cache-plugin';
+import * as CacheDriver from '@groupby/elements-cache-driver-plugin';
 import * as Core from '@groupby/elements-core';
-import { expect, stub } from '../../utils';
+import * as DomEvents from '@groupby/elements-dom-events-plugin';
+import * as Sayt from '@groupby/elements-sayt-plugin';
+import * as SaytDriver from '@groupby/elements-sayt-driver-plugin';
+import * as Search from '@groupby/elements-search-plugin';
+import * as SearchDriver from '@groupby/elements-search-driver-plugin';
+import { expect, spy, stub } from '../../utils';
 import quickStart from '../../../src/quick-start';
 
 describe('quickStart()', () => {
-  it('should return an instance of Core', () => {
-    const core = { a: 'a' };
-    const CoreStub = stub(Core, 'Core').returns(core);
+  let CoreStub;
+  let CachePlugin;
+  let CacheDriverPlugin;
+  let DomEventsPlugin;
+  let SaytPlugin;
+  let SaytDriverPlugin;
+  let SearchPlugin;
+  let SearchDriverPlugin;
+  let core;
+  let register;
 
+  beforeEach(() => {
+    register = spy();
+    core = { register };
+    CoreStub = stub(Core, 'Core').returns(core);
+    CachePlugin = stub(Cache, 'CachePlugin');
+    CacheDriverPlugin = stub(CacheDriver, 'CacheDriverPlugin');
+    DomEventsPlugin = stub(DomEvents, 'DomEventsPlugin');
+    SaytPlugin = stub(Sayt, 'SaytPlugin');
+    SaytDriverPlugin = stub(SaytDriver, 'SaytDriverPlugin');
+    SearchPlugin = stub(Search, 'SearchPlugin');
+    SearchDriverPlugin = stub(SearchDriver, 'SearchDriverPlugin');
+  });
+
+  it('should return an instance of Core', () => {
     const returnedCore = quickStart();
 
     expect(CoreStub.calledWithNew()).to.be.true;
     expect(returnedCore).to.equal(core);
+  });
+
+  it('should instantiate plugins', () => {
+    quickStart();
+
+    expect(CachePlugin.calledWithNew()).to.be.true;
+    expect(CacheDriverPlugin.calledWithNew()).to.be.true;
+    expect(DomEventsPlugin.calledWithNew()).to.be.true;
+    expect(SaytPlugin.calledWithNew()).to.be.true;
+    expect(SaytDriverPlugin.calledWithNew()).to.be.true;
+    expect(SearchPlugin.calledWithNew()).to.be.true;
+    expect(SearchDriverPlugin.calledWithNew()).to.be.true;
   });
 
   it('should registers plugins');
