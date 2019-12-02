1b7b58a48d2d56a4205bb4f922c132f9fedd6413
Johann Tutor <johann.tutor@groupbyinc.com>
2019-12-02 11:34:08 -0500

    Add stub tests

---
+++ packages/@groupby/elements-quickstart/test/unit/common/quick-start.test.ts
@@ -0,0 +1,9 @@
+import quickStart from '../../../src/quick-start';
+
+describe('quickStart()', () => {
+  it('should instantiate Core');
+  it('should registers plugins');
+  it('should forward customerId to SaytPlugin');
+  it('should forward productTransformer to SaytPlugin');
+  it('should forward productTransformer to SearchPlugin');
+});
