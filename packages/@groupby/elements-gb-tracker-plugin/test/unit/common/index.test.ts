fee215053965eb85a73165d113f465874b29d087
Daniel Moniz <daniel.moniz@groupbyinc.com>
2019-12-06 16:31:17 -0500

    Fix index.js test for GbTrackerPlugin

--- packages/@groupby/elements-gb-tracker-plugin/test/unit/common/index.test.ts
+++ packages/@groupby/elements-gb-tracker-plugin/test/unit/common/index.test.ts
@@ -3,7 +3,7 @@ import GbTrackerPlugin from '../../../src/gb-tracker-plugin';
 import { GbTrackerPlugin as GbTrackerExport } from '../../../src/index';
 
 describe('Entry point', () => {
-  it('should export SaytPlugin', () => {
+  it('should export GbTrackerPlugin', () => {
     expect(GbTrackerPlugin).to.equal(GbTrackerExport);
   });
 });
