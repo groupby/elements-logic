2117f9778c9d17ad24fde7b91dbf4cb5e2521af3
Geoff Coutts <geoff.coutts@gmail.com>
2019-12-06 13:29:33 -0500

    Add test for exporting SaytDriverOptions interface

--- packages/@groupby/elements-sayt-driver-plugin/test/unit/common/index.test.ts
+++ packages/@groupby/elements-sayt-driver-plugin/test/unit/common/index.test.ts
@@ -1,9 +1,14 @@
-import { expect } from '../../utils';
-import { SaytDriverPlugin as SaytDriverPluginExport } from '../../../src';
-import SaytDriverPlugin from '../../../src/sayt-driver-plugin';
+import { AssertTypesEqual, expect } from '../../utils';
+import { SaytDriverPlugin as SaytDriverPluginExport, SaytDriverOptions as SaytDriverOptionsExport } from '../../../src';
+import SaytDriverPlugin, { SaytDriverOptions } from '../../../src/sayt-driver-plugin';
 
 describe('Entry point', () => {
   it('should export the SaytDriverPlugin', () => {
     expect(SaytDriverPluginExport).to.equal(SaytDriverPlugin);
+  });
+
+  it('should export the SaytDriverOptions interface', () => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const test: AssertTypesEqual<SaytDriverOptionsExport<{}>, SaytDriverOptions<{}>> = true;
   });
 });
