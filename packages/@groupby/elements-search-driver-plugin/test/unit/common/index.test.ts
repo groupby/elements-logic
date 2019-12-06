c366ce4ed57d7c370e224ebf220b0578ed2f95da
Geoff Coutts <geoff.coutts@gmail.com>
2019-12-06 13:34:48 -0500

    Add test for SearchDriverOptions export

--- packages/@groupby/elements-search-driver-plugin/test/unit/common/index.test.ts
+++ packages/@groupby/elements-search-driver-plugin/test/unit/common/index.test.ts
@@ -1,9 +1,17 @@
-import { expect } from '../../utils';
-import SearchDriverPlugin from '../../../src/search-driver-plugin';
-import { SearchDriverPlugin as SearchDriverExport } from '../../../src/index';
+import { AssertTypesEqual, expect } from '../../utils';
+import SearchDriverPlugin, { SearchDriverOptions } from '../../../src/search-driver-plugin';
+import {
+  SearchDriverPlugin as SearchDriverExport,
+  SearchDriverOptions as SearchDriverOptionsExport,
+} from '../../../src/index';
 
 describe('Entry point', () => {
   it('should export SearchDriverPlugin', () => {
     expect(SearchDriverExport).to.equal(SearchDriverPlugin);
+  });
+
+  it('should export the SearchDriverOptions interface', () => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const test: AssertTypesEqual<SearchDriverOptionsExport<{}>, SearchDriverOptions<{}>> = true;
   });
 });
