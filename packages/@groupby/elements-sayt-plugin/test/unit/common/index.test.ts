0be6a31506a77444b8add9d3015c63008e0c9326
Geoff Coutts <geoff.coutts@gmail.com>
2019-12-06 11:35:59 -0500

    Add test for SaytOptions export

--- packages/@groupby/elements-sayt-plugin/test/unit/common/index.test.ts
+++ packages/@groupby/elements-sayt-plugin/test/unit/common/index.test.ts
@@ -1,9 +1,14 @@
-import { expect } from '../../utils';
-import SaytPlugin from '../../../src/sayt-plugin';
-import { SaytPlugin as SaytExport } from '../../../src/index';
+import { AssertTypesEqual, expect } from '../../utils';
+import SaytPlugin, { SaytOptions } from '../../../src/sayt-plugin';
+import { SaytPlugin as SaytExport, SaytOptions as SaytOptionsExport } from '../../../src/index';
 
 describe('Entry point', () => {
   it('should export SaytPlugin', () => {
     expect(SaytPlugin).to.equal(SaytExport);
+  });
+
+  it('should export the SaytOptions interface', () => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const test: AssertTypesEqual<SaytOptionsExport, SaytOptions> = true;
   });
 });
