7211b6cef91e62789c5d464a489838c1c7aaedd8
Daniel Moniz <daniel.moniz@groupbyinc.com>
2019-12-11 09:54:29 -0500

    Add TrackerPluginOptions as export

--- packages/@groupby/elements-gb-tracker-plugin/test/unit/common/index.test.ts
+++ packages/@groupby/elements-gb-tracker-plugin/test/unit/common/index.test.ts
@@ -1,9 +1,17 @@
-import { expect } from '../../utils';
-import GbTrackerPlugin from '../../../src/gb-tracker-plugin';
-import { GbTrackerPlugin as GbTrackerExport } from '../../../src/index';
+import { AssertTypesEqual, expect } from '../../utils';
+import GbTrackerPlugin, { TrackerPluginOptions } from '../../../src/gb-tracker-plugin';
+import {
+  GbTrackerPlugin as GbTrackerPluginExport,
+  TrackerPluginOptions as GbTrackerOptionsExport,
+} from '../../../src/index';
 
 describe('Entry point', () => {
   it('should export GbTrackerPlugin', () => {
-    expect(GbTrackerPlugin).to.equal(GbTrackerExport);
+    expect(GbTrackerPlugin).to.equal(GbTrackerPluginExport);
+  });
+
+  it('should export TrackerPluginOptions', () => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const test: AssertTypesEqual<GbTrackerOptionsExport, TrackerPluginOptions> = true;
   });
 });
